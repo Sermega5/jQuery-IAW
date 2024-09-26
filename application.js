69f019ca2ddd6eae1f9ac5d771cb2160b432a2e1
Sermega <ser.matossi@gmail.com>
2024-09-26 14:25:04 +0200

    actualizado

--- application.js
+++ application.js
@@ -1,3 +1,6 @@
-$(document).ready(function () {
-    $("h1").text("Artean Matossi")
+jQuery(document).ready(function () {
+    $("#categorias li:first").text("TL")
+
+    var nuevocurso = $('<li>RJA</li>')
+    $("#categorias li:last").after(nuevocurso);
 });
